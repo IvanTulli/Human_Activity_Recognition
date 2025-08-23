77621d430acd0262bff5b51b97ae28d44c35550a
IvanTulli <ivantulli@gmail.com>
2025-08-23 21:33:54 +0100

    Added an SVM model and improved other things

--- Streamlit App/Human_Activity_Classification_app.py
+++ Streamlit App/Human_Activity_Classification_app.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.pipeline import make_pipeline
@@ -29,7 +29,12 @@ import pickle
 def main():
     st.title('Human Activity Classification via Smartphone Readings')
     st.sidebar.title('Multiclass Classification Web App')
-    st.markdown('Determining whether someone is standing, laying, sitting, walking, walking downstairs, or walking upstairs.')
+    with st.expander("Information"):
+        st.markdown('We are using the Kaggle dataset: https://www.kaggle.com/datasets/uciml/human-activity-recognition-with-smartphones.')
+        st.markdown("The aim is, given the gyroscope and accelerometer readings of a smartphone, to determine whether a person is laying, "
+        "sitting, standing, walking, walking downstairs, or walking upstairs.")
+        st.markdown("This app shows the result metrics (accuracy, recall, precision, f1score) on the test set, either on a model trained by the app, "
+        "or by a model that is pre-trained.")
 
     @st.cache_data
     def load_data():
@@ -45,6 +50,18 @@ def main():
         y_test  = df_test.iloc[:, -1]
         return X_train, X_test, y_train, y_test
 
+    def predict(model, x_test):
+        y_pred = model.predict(x_test)
+
+        acc = accuracy_score(y_test, y_pred)
+        report = classification_report(
+                    y_test, y_pred, labels=labels, output_dict=True, zero_division=0
+                    )
+        df_report = pd.DataFrame(report).transpose().round(3)
+        return acc, df_report, y_pred
+
+
+
     def plot_conf_matrix(y_true, y_pred, label_names):
         cm = confusion_matrix(y_true, y_pred, labels=label_names)
         fig, ax = plt.subplots(figsize=(5, 5))
@@ -55,12 +72,12 @@ def main():
         st.pyplot(fig)
 
     X_train, X_test, y_train, y_test = load_data()
-    labels = sorted(y_train.unique().tolist())   # explicit class order
+    labels = sorted(y_train.unique().tolist())   
     trainchoice= st.sidebar.selectbox("Make a choice:", ("-", "Train a new model", "Use a trained model"))
     if trainchoice == "Train a new model":
         clf_choice = st.sidebar.selectbox(
             'Classifier',
-            ('Random Choice (Baseline)', 'Logistic Regression', 'Random Forest')
+            ('-','Random Choice (Baseline)', 'Support Vector Machine', 'Logistic Regression', 'Random Forest')
         )
 
         plot_cm_choice = st.sidebar.radio("Plot the confusion matrix?", ('Yes', 'No'))
@@ -85,6 +102,33 @@ def main():
                 if plot_cm_choice == 'Yes':
                     plot_conf_matrix(y_test, y_pred, labels)
 
+        if clf_choice == 'Support Vector Machine':
+            st.sidebar.subheader("Model Hyperparameters")
+            C = st.sidebar.number_input("C (Regularization)", 0.01, 10.0, value=1.0, step=0.01, key='C_SVM')
+            kernel = st.sidebar.radio('Kernel', ('linear', 'rbf', 'poly'))
+            if st.sidebar.button("Classify", key='classify_svm'):
+                st.subheader("Support Vector Machine Results")
+
+                with st.spinner("Training model..."):
+                
+                    model = make_pipeline(
+                        StandardScaler(),
+                        SVC(C=C, kernel=kernel)
+                    )
+                    model.fit(X_train, y_train)
+                    time.sleep(0.5)  
+                    st.success("Training complete!")
+                with st.spinner("Making predictions..."):
+                    time.sleep(0.5)
+
+                    acc, df_report, y_pred = predict(model, X_test)
+
+                    st.write(f"Accuracy: **{acc:.3f}**")
+                    st.table(df_report.loc[labels, ['precision', 'recall', 'f1-score']])
+
+                if plot_cm_choice == 'Yes':
+                    plot_conf_matrix(y_test, y_pred, labels)
+
         if clf_choice == 'Logistic Regression':
             st.sidebar.subheader("Model Hyperparameters")
             C = st.sidebar.number_input("C (Regularization)", 0.01, 10.0, value=1.0, step=0.01, key='C_LR')
@@ -100,19 +144,16 @@ def main():
                     model.fit(X_train, y_train)
                     time.sleep(0.5)  
                     st.success("Training complete!")
-                y_pred = model.predict(X_test)
+                
+                with st.spinner("Making predictions..."):
+                    time.sleep(0.5)
+                    acc, df_report, y_pred = predict(model, X_test)
 
-                acc = accuracy_score(y_test, y_pred)
-                report = classification_report(
-                    y_test, y_pred, labels=labels, output_dict=True, zero_division=0
-                )
-                df = pd.DataFrame(report).transpose().round(3)
+                    st.write(f"Accuracy: **{acc:.3f}**")
+                    st.table(df_report.loc[labels, ['precision', 'recall', 'f1-score']])
 
-                st.write(f"Accuracy: **{acc:.3f}**")
-                st.table(df.loc[labels, ['precision', 'recall', 'f1-score']])
-
-                if plot_cm_choice == 'Yes':
-                    plot_conf_matrix(y_test, y_pred, labels)
+                    if plot_cm_choice == 'Yes':
+                        plot_conf_matrix(y_test, y_pred, labels)
 
         if clf_choice == 'Random Forest':
             st.sidebar.subheader("Model Hyperparameters")
@@ -130,35 +171,45 @@ def main():
                     )
                     rf.fit(X_train, y_train)
                     st.success("Training Complete!")
-                y_pred = rf.predict(X_test)
+                
+                with st.spinner("Making predictions..."):
+                    time.sleep(0.5)
+                    acc, df_report, y_pred = predict(rf, X_test)
 
-                acc = accuracy_score(y_test, y_pred)
-                report = classification_report(
-                    y_test, y_pred, labels=labels, output_dict=True, zero_division=0
-                )
-                df = pd.DataFrame(report).transpose().round(3)
+                    st.write(f"Accuracy: **{acc:.3f}**")
+                    st.table(df_report.loc[labels, ['precision', 'recall', 'f1-score']])
+
+                    if plot_cm_choice == 'Yes':
+                        plot_conf_matrix(y_test, y_pred, labels)
+
+    elif trainchoice == "Use a trained model":
+        clf_choice = st.sidebar.selectbox('Choose trained classifier', ('-', 'Support Vector Machine', 'Logistic Regression', 'Random Forest'))
+        plot_cm_choice = st.sidebar.radio("Plot the confusion matrix?", ('Yes', 'No'))
+        if clf_choice == "Support Vector Machine":
+            with open("svm_model.pkl", "rb") as f:
+                loaded_svm = pickle.load(f)
+            if st.sidebar.button("Predict", key = "predict_svm"):
+                st.subheader('Support Vector Machine Predictions')
+                y_pred = loaded_svm.predict(X_test)
+
+                acc, df_report, y_pred = predict(loaded_svm, X_test)
 
                 st.write(f"Accuracy: **{acc:.3f}**")
-                st.table(df.loc[labels, ['precision', 'recall', 'f1-score']])
+                st.table(df_report.loc[labels, ['precision', 'recall', 'f1-score']])
 
                 if plot_cm_choice == 'Yes':
                     plot_conf_matrix(y_test, y_pred, labels)
-    elif trainchoice == "Use a trained model":
-        clf_choice = st.sidebar.selectbox('Choose trained classifier', ('Logistic Regression', 'Random Forest'))
-        plot_cm_choice = st.sidebar.radio("Plot the confusion matrix?", ('Yes', 'No'))
         if clf_choice == "Logistic Regression":
             with open("lr_model.pkl", "rb") as f:
                 loaded_lr = pickle.load(f)
             if st.sidebar.button("Predict", key = "predict_lr"):
+                st.subheader("Logistic Regressions Predictions")
                 y_pred = loaded_lr.predict(X_test)
 
-                acc = accuracy_score(y_test, y_pred)
-                report = classification_report(
-                        y_test, y_pred, labels=labels, output_dict=True, zero_division=0)
-                df = pd.DataFrame(report).transpose().round(3)
+                acc, df_report, y_pred = predict(loaded_lr, X_test)
 
                 st.write(f"Accuracy: **{acc:.3f}**")
-                st.table(df.loc[labels, ['precision', 'recall', 'f1-score']])
+                st.table(df_report.loc[labels, ['precision', 'recall', 'f1-score']])
 
                 if plot_cm_choice == 'Yes':
                     plot_conf_matrix(y_test, y_pred, labels)
@@ -167,15 +218,13 @@ def main():
             with open("rf_model.pkl", "rb") as f:
                 loaded_rf = pickle.load(f)
             if st.sidebar.button("Predict", key = "predict_rf"):
+                st.subheader("Random Forest Predictions")
                 y_pred = loaded_rf.predict(X_test)
 
-                acc = accuracy_score(y_test, y_pred)
-                report = classification_report(
-                        y_test, y_pred, labels=labels, output_dict=True, zero_division=0)
-                df = pd.DataFrame(report).transpose().round(3)
+                acc, df_report, y_pred = predict(loaded_rf, X_test)
 
                 st.write(f"Accuracy: **{acc:.3f}**")
-                st.table(df.loc[labels, ['precision', 'recall', 'f1-score']])
+                st.table(df_report.loc[labels, ['precision', 'recall', 'f1-score']])
 
                 if plot_cm_choice == 'Yes':
                     plot_conf_matrix(y_test, y_pred, labels)
