5b00ad6ee43574d9a8b51e63992ae906203a94d9
IvanTulli <ivantulli@gmail.com>
2025-08-24 15:36:59 +0100

    added requirements.txt and some more minor modifications to the python script

--- Streamlit App/Human_Activity_Classification_app.py
+++ Streamlit App/Human_Activity_Classification_app.py
@@ -4,9 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
